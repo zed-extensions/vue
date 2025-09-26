596a0c79226fd49d288fcc826235016d69ba5fba
Max Brunsfeld <maxbrunsfeld@gmail.com>
2025-09-26 12:16:01 -0700

    Bump extension api, remove windows path workaround (#54)
    ⚠️ Don't merge until Zed 0.205.x is on stable ⚠️ 
    
    See https://github.com/zed-industries/zed/pull/37811
    
    This PR bumps the zed extension API to the latest version, which removes
    the need to work around a bug where `current_dir()` returned an invalid
    path on windows.

--- src/vue.rs
+++ src/vue.rs
@@ -120,7 +120,8 @@ impl VueExtension {
             println!("typescript already installed");
         }
 
-        self.typescript_tsdk_path = zed_ext::sanitize_windows_path(env::current_dir().unwrap())
+        self.typescript_tsdk_path = env::current_dir()
+            .unwrap()
             .join(TYPESCRIPT_TSDK_PATH)
             .to_string_lossy()
             .to_string();
@@ -182,7 +183,8 @@ impl zed::Extension for VueExtension {
         Ok(zed::Command {
             command: zed::node_binary_path()?,
             args: vec![
-                zed_ext::sanitize_windows_path(env::current_dir().unwrap())
+                env::current_dir()
+                    .unwrap()
                     .join(&server_path)
                     .to_string_lossy()
                     .to_string(),
@@ -292,25 +294,3 @@ impl zed::Extension for VueExtension {
 }
 
 zed::register_extension!(VueExtension);
-
-/// Extensions to the Zed extension API that have not yet stabilized.
-mod zed_ext {
-    /// Sanitizes the given path to remove the leading `/` on Windows.
-    ///
-    /// On macOS and Linux this is a no-op.
-    ///
-    /// This is a workaround for https://github.com/bytecodealliance/wasmtime/issues/10415.
-    pub fn sanitize_windows_path(path: std::path::PathBuf) -> std::path::PathBuf {
-        use zed_extension_api::{current_platform, Os};
-
-        let (os, _arch) = current_platform();
-        match os {
-            Os::Mac | Os::Linux => path,
-            Os::Windows => path
-                .to_string_lossy()
-                .to_string()
-                .trim_start_matches('/')
-                .into(),
-        }
-    }
-}
