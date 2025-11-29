f0cb1fb76acd733dd9fd1bf7a3aa54af526ad888
aizigao <weixiangzhe@live.com>
2025-11-29 22:58:58 +0800

    feat: delete useless blocks

--- languages/vue/outline.scm
+++ languages/vue/outline.scm
@@ -4,7 +4,6 @@
 ; ========== template =========
 (template_element
   (start_tag) @name
-  (attribute (attribute_name) @attr_name (quoted_attribute_value (attribute_value) @attr_value))*
 ) @item
 
 
@@ -24,16 +23,10 @@
 
 (script_element
     (start_tag) @name
-    (raw_text) @context @item
-)
-
-(script_element
-    (end_tag) @name @item
-)
+) @item
 
 
 ; ========= style =========
 (style_element
     (start_tag) @name
-    (raw_text) @context
 ) @item
