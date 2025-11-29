e671b08dc9b2bb9497d8bab326eb88567e60151a
aizigao <weixiangzhe@live.com>
2025-11-29 16:10:35 +0800

    feat: update outline config

--- languages/vue/outline.scm
+++ languages/vue/outline.scm
@@ -1,46 +1,39 @@
-
-
 ; Comments appear as annotations in the outline
 (comment) @annotation
 
 ; ========== template =========
 (template_element
-  (start_tag
-    (tag_name) @name
-    (attribute
-      (attribute_name) @_lang
-      (quoted_attribute_value (attribute_value) @annotation))?)
-  (#eq? @_lang "lang")) @item
-
-; Custom elements/components (PascalCase or hyphenated) - self-closing tags
-(
-  (element
-    (self_closing_tag
-      (tag_name) @name))
-  (#match? @name "^[A-Z]|-")
+  (start_tag) @name
+  (attribute (attribute_name) @attr_name (quoted_attribute_value (attribute_value) @attr_value))*
 ) @item
 
-; normal tags
+
+; Custom elements/components (PascalCase or hyphenated) - self-closing tags
 (element
   (start_tag
-    (tag_name) @name)) @item
+    (tag_name) @name)
+) @item
+
+(element
+  (self_closing_tag
+    (tag_name) @name)
+) @item
+
 
 ; ======= script ======
 
 (script_element
-  (start_tag
-    (tag_name) @name
-    (attribute
-      (attribute_name) @_lang
-      (quoted_attribute_value (attribute_value) @annotation))?)
-  (#eq? @_lang "lang")) @item
+    (start_tag) @name
+    (raw_text) @context @item
+)
+
+(script_element
+    (end_tag) @name @item
+)
 
 
 ; ========= style =========
 (style_element
-  (start_tag
-    (tag_name) @name
-    (attribute
-      (attribute_name) @_lang
-      (quoted_attribute_value (attribute_value) @annotation))?)
-  (#eq? @_lang "lang")) @item
+    (start_tag) @name
+    (raw_text) @context
+) @item
